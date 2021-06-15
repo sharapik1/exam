632af536f7bd1fcfda8dfb123eeda598255827e7
sharapik1 <sharapik242424@gmail.com>
2021-06-15 16:28:25 +0300

    Подготовка4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,9 +17,49 @@ using WpfApp2;
 
 namespace WpfApp2
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public string SelectedCity = "";
+        public List<City> CityList { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private IEnumerable<Pokipateli> _PokipateliList = null;
+        
+        public IEnumerable<Pokipateli> PokipateliList
+        {
+            get
+            {
+                return _PokipateliList
+                    .Where(c => (SelectedCity == "Все города" || c.City == SelectedCity));
+            }
+            set
+            {
+                _PokipateliList = value;
+            }
+        }
 
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Globals.dataProvider = new LocalDataProvider();
+            PokipateliList = Globals.dataProvider.GetPokipatelis();
+            CityList = Globals.dataProvider.GetCities().ToList();
+            CityList.Insert(0, new City { Title = "Все города" });
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Invalidate()
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("CityList"));
+        }
+        private void CityFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedCity = (CityFilterComboBox.SelectedItem as City).Title;
+            Invalidate();
+        }
     }
 }
 
---
+++ Model/LocalDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class LocalDataProvider : IDataProvider
+    {
+        public IEnumerable<Pokipateli> GetPokipatelis()
+        {
+            return new Pokipateli[] {
+            new Pokipateli {Name="Павел", Familia="Пригорков", Age=30, Symmapokipok=680.20, Data=new DateTime(2021,05,21), City="Йошкар-Ола"},
+            new Pokipateli {Name="Михаил", Familia="Григорьев", Age=31, Symmapokipok=5747.30, Data=new DateTime(2020,08,21), City="Москва"},
+            new Pokipateli {Name ="Петр", Familia = "Михайлович", Age =26, Symmapokipok=2000.60, Data=new DateTime(2021,05,20), City="Уфа"}};
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return new City[]
+            {
+                new City { Title="Йошкар-Ола"},
+                new City { Title="Москва"},
+                new City { Title="Уфа"},
+            };
+
+        }
+    }
+}
