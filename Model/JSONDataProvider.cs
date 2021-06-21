f9f4fa4a8b23b557fffa66d2aeaa69e8be550266
skocty <skocty@bk.ru>
2021-06-21 10:48:28 +0300

    Экзамен2

---
+++ Model/JSONDataProvider.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Model
+{
+ 
+   
+
+    public class JSONDataProvider : IDataProvider
+    {
+        private pokipateli[] PokipateliList;
+        //private List<pokipateli> PokipateliList = new List<pokipateli>();
+
+        public JSONDataProvider(String fileName)
+        {
+            var Serializer = new DataContractJsonSerializer(typeof(pokipateli[]));
+            using (var sr =  new StreamReader(fileName))
+            {
+                PokipateliList = (pokipateli[])Serializer.ReadObject(sr.BaseStream);
+            }
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return new City[]
+            {
+                new City { Title="Йошкар-Ола"},
+                new City { Title="Москва"},
+                new City { Title="Уфа"}
+            };
+        }
+
+        public IEnumerable<pokipateli> GetPokipatelis()
+        {
+            return PokipateliList;
+        }
+    }
+}
--- Model/pokipateli.cs
+++ Model/pokipateli.cs
@@ -1,27 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp2.Model
 {
-    public class pokipateli
+    [DataContract]
+    public  class pokipateli
     {
+        private DateTime privateDate;
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public string Familia { get; set; }
+        [DataMember]
         public int Age { get; set; }
+        [DataMember]
         public double Symmapokipok { get; set; }
-        public DateTime Data { get; set; }
-        public string City { get; set; }
 
+       
+        [DataMember]
+        public string City { get; set; }
+        [DataMember(Name = "Date")]
+        public string StringDate
+        {
+            get
+            {
+
+                return privateDate.ToString("yyyy-MM-dd");
+            }
+            set
+            {
+                privateDate = new DateTime(
+                    Convert.ToInt32(value.Substring(0, 4)),
+                    Convert.ToInt32(value.Substring(5, 2)),
+                    Convert.ToInt32(value.Substring(8, 2))
+                    );
+            }
+
+        }
+        [IgnoreDataMember]
+        public DateTime Date
+        {
+            get
+            {
+                return privateDate;
+            }
+            set
+            {
+                privateDate = value;
+            }
+        }
         public string DateString
         {
             get
             {
-                return Data.ToShortDateString();
+                return Date.ToShortDateString();
             }
         }
+
 
     }
 }
